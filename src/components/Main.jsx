0c7e2162dcd31fb5821393b44783e7bb84a09558
AndreyNguen <goonikjke@gmail.com>
2022-12-22 11:31:43 +0300

    home v2

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Cards from './catalog/Cards';
+import Orderform from './orderForm/Orderform';
 
 function Main() {
   return (
@@ -12,26 +13,28 @@ function Main() {
         </div>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src="https://catherineasquithgallery.com/uploads/posts/2021-02/1614434047_62-p-chasi-na-temnom-fone-69.jpg" className="d-block w-100" alt="no img" />
+            <img src="https://img1.goodfon.com/original/1920x1200/3/fd/omega-seamaster-omega-4395.jpg" className="d-block w-100" alt="no img" />
             <div className="carousel-caption d-none d-md-block">
               <h5>Вы вдохновляете нас вашими идеями</h5>
               <p>You inspire us with your ideas</p>
             </div>
           </div>
           <div className="carousel-item">
-            <img src="https://c.wallhere.com/photos/82/d4/aviation_hamilton_watch_wristwatch-563969.jpg!d" className="d-block w-100" alt="no img" />
+            <img src="https://img3.goodfon.ru/original/1920x1200/b/d8/chasy-budilnik-ciferblat.jpg" className="d-block w-100" alt="no img" />
             <div className="carousel-caption d-none d-md-block">
               <h5>Вместе мы покорим мир часов</h5>
               <p>Together we will conquer the world of watches</p>
             </div>
           </div>
+
           <div className="carousel-item">
-            <img src="https://img1.goodfon.ru/original/2880x1800/a/a0/chasy-ulysse-nardin-blue-toro.jpg" className="d-block w-100" alt="no img" />
+            <img src="https://img5.goodfon.com/original/1920x1200/f/df/chasy-shveitsarskie-muzhskie-naruchnye-breget-breguet-montre.jpg" className="d-block w-100" alt="no img" />
             <div className="carousel-caption d-none d-md-block">
               <h5>Ваша идея - наша реализация</h5>
               <p>Your idea is our implementation</p>
             </div>
           </div>
+
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
           <span className="carousel-control-prev-icon" aria-hidden="true" />
@@ -42,19 +45,28 @@ function Main() {
           <span className="visually-hidden">Следующий</span>
         </button>
         <br />
+      </div>
 
-      </div>
-      <div
-        className="footer"
-        id="footer"
-        style={{
-          width: '1300px', height: '150px', color: '#fff', backgroundColor: '#000000',
-        }}
-      >
-        <footer>
-          <p>© 2018 Watch shop</p>
-        </footer>
-      </div>
+      <Cards />
+      <Orderform />
+      <br />
+      <footer>
+        <div className="info">
+          <div className="info_links">
+            <p>© 1999–2022 интернет-магазин наручных часов Watch-shop.ru</p>
+          </div>
+
+        </div>
+      </footer>
+
+      {/* <div className="feet-item">
+        <div id="footer">
+          <footer>
+            <p>© 2018 Watch shop</p>
+          </footer>
+        </div>
+      </div> */}
+
     </>
   );
 }
